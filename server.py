a5bd27981aee27eb56585b3b89877742027b36d3
toddbeckman-wf <todd.beckman@workiva.com>
2017-04-22 16:53:16 -0600

    url safe base64 doc id

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
+import base64
 from bottle import Bottle, get, request, response, run
 
 app = Bottle()
 
 sampledocs = {
-    '1': 'something glorious',
-    '2': 'something enigmatic',
-    'a': 'something fantastic',
+    'a-glorious-doc': 'something glorious',
+    'an-enigmatic-doc': 'something enigmatic',
+    'a-fantastic-doc': 'something fantastic',
 }
 
+def decodeBase64Url(url):
+    return base64.urlsafe_b64decode(url.replace("~", "="))
 
 @app.hook('after_request')
 def enable_corse():
@@ -18,21 +21,31 @@ def enable_corse():
     response.headers['Access-Control-Allow-Methods'] = 'GET'
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
+@app.route('/doc/list', method=['GET'])
+def getDocListHandler():
+    """
+    Get the list of documents
+    """
+    return sampledocs.keys()
+
 @app.route('/doc/<docid>', method=['GET'])
 def getDocHandler(docid):
     """
     Let KeyError raise for now. Error handling later.
     """
-    return sampledocs[docid]
+    print(docid)
+    decodedDocId = decodeBase64Url(docid)
+    return sampledocs[decodedDocId]
 
 @app.route('/doc/<docid>', method=['POST'])
 def postDocHandler(docid):
     """
     Naively insert
     """
+    decodedDocId = decodeBase64Url(docid)
     postdata = request.body.read()
-    print docid
+    print decodedDocId
     print postdata
-    sampledocs[docid] = postdata
+    sampledocs[decodedDocId] = postdata
 
 run(app, host='localhost', port=5080)
